0318c8d2d92a32f85931c332da82b5c7817720b5
666 <@4d.666>
2018-03-26 21:46:46 +0800

    san

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const router =require('./router')
 
 const app =express()
 
+//开放静态资源
+app.use('/public',express.static('./public/'))
+app.use('/node_modules',express.static('./node_modules/'))
+
+//配置使用模板引擎
+app.engine('html',require('express-art-template'))
+
 //使用路由模块
 app.use(router)
 
